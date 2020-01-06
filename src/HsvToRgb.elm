9f4b55d54c8bcfffc3d8617fc152a7be41489529
wcourtney31415 <wcourtney31415@gmail.com>
2020-01-06 02:29:53 -0500

    Extracted the conversion logic to it's own module file. Added a button to trigger the conversion. Made some formatting changes to the view. Successfully compiled the application, and performed the calculation via the button press. Unfortunately I've discovered the conversion algorithm isn't working properly. I will have to address this, but for now other than that logical error, it seems to be working.

---
+++ src/HsvToRgb.elm
@@ -0,0 +1,115 @@
+module HsvToRgb exposing (Data, hsvToRgb)
+
+
+type alias Input =
+    { h : Int
+    , s : Float
+    , v : Float
+    }
+
+
+type alias Data =
+    { h : Float
+    , s : Float
+    , v : Float
+    , r : Float
+    , g : Float
+    , b : Float
+    , c : Float
+    , x : Float
+    , m : Float
+    }
+
+
+type alias Results =
+    { r : Int
+    , g : Int
+    , b : Int
+    }
+
+
+dataIncluding : Input -> Data
+dataIncluding input =
+    { h = toFloat input.h
+    , s = input.s
+    , v = input.v
+    , r = 0
+    , g = 0
+    , b = 0
+    , c = 0
+    , x = 0
+    , m = 0
+    }
+
+
+hsvToRgb : Input -> Results
+hsvToRgb input =
+    solveRGB <| calcRange <| solveM <| solveX <| solveC <| dataIncluding input
+
+
+solveC : Data -> Data
+solveC data =
+    { data | c = data.v * data.s }
+
+
+solveM : Data -> Data
+solveM data =
+    { data | m = data.v - data.c }
+
+
+solveX : Data -> Data
+solveX data =
+    { data | x = data.c * (1 - abs (floatModulous (data.h / 60) 2 0 - 1)) }
+
+
+floatModulous : Float -> Float -> Int -> Float
+floatModulous destination divisor iterator =
+    if ((divisor * toFloat iterator) + 1) > destination then
+        destination - (divisor * toFloat iterator)
+
+    else
+        floatModulous destination divisor (iterator + 1)
+
+
+calcRange : Data -> Data
+calcRange data =
+    if 0 <= data.h && data.h < 60 then
+        { data | r = data.c, g = data.x, b = 0 }
+
+    else if 60 <= data.h && data.h < 120 then
+        { data | r = data.x, g = data.c, b = 0 }
+
+    else if 120 <= data.h && data.h < 180 then
+        { data | r = 0, g = data.c, b = data.x }
+
+    else if 180 <= data.h && data.h < 240 then
+        { data | r = 0, g = data.x, b = data.c }
+
+    else if 240 <= data.h && data.h < 300 then
+        { data | r = data.x, g = 0, b = data.c }
+
+    else if 300 <= data.h && data.h < 360 then
+        { data | r = data.c, g = 0, b = data.x }
+
+    else
+        data
+
+
+unprime : Float -> Float -> Float
+unprime myColor m =
+    (myColor + m) * 255
+
+
+solveRGB : Data -> Results
+solveRGB data =
+    let
+        myR =
+            round <| unprime data.r data.m
+
+        myG =
+            round <| unprime data.g data.m
+
+        myB =
+            round <| unprime data.b data.m
+    in
+    { r = myR, g = myG, b = myB }
--- src/Main.elm
+++ src/Main.elm
@@ -3,25 +3,28 @@ module Main exposing (..)
 import Browser
 import Element exposing (..)
 import Element.Background as Background
+import Element.Border as Border
 import Element.Font as Font
 import Element.Input as Input
+import HsvToRgb exposing (hsvToRgb)
 import Html exposing (Html)
 
 
 main =
-    Browser.sandbox { init = init, update = update, view = view }
+    Browser.sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
 
 
 type alias Model =
-    { h : Float
+    { h : Int
     , s : Float
     , v : Float
-    , r : Float
-    , g : Float
-    , b : Float
-    , c : Float
-    , x : Float
-    , m : Float
+    , r : Int
+    , g : Int
+    , b : Int
     }
 
 
@@ -33,111 +36,77 @@ init =
     , r = 0
     , g = 0
     , b = 0
-    , c = 0
-    , x = 0
-    , m = 0
     }
 
 
-hsvToRgb : Model -> Model
-hsvToRgb model =
-    unprimeRGB <| calcRange <| calcM <| calcX <| calcC model
-
-
-calcC : Model -> Model
-calcC model =
-    { model | c = model.v * model.s }
-
-
-calcM : Model -> Model
-calcM model =
-    { model | m = model.v - model.c }
-
-
-calcX : Model -> Model
-calcX model =
-    { model | x = model.c * (1 - abs (floatModulous (model.h / 60) 2 0 - 1)) }
-
-
-floatModulous : Float -> Float -> Int -> Float
-floatModulous destination divisor iterator =
-    if ((divisor * toFloat iterator) + 1) > destination then
-        destination - (divisor * toFloat iterator)
-
-    else
-        floatModulous destination divisor (iterator + 1)
-
-
-calcRange : Model -> Model
-calcRange model =
-    if 0 <= model.h && model.h < 60 then
-        { model | r = model.c, g = model.x, b = 0 }
-
-    else if 60 <= model.h && model.h < 120 then
-        { model | r = model.x, g = model.c, b = 0 }
-
-    else if 120 <= model.h && model.h < 180 then
-        { model | r = 0, g = model.c, b = model.x }
-
-    else if 180 <= model.h && model.h < 240 then
-        { model | r = 0, g = model.x, b = model.c }
-
-    else if 240 <= model.h && model.h < 300 then
-        { model | r = model.x, g = 0, b = model.c }
-
-    else if 300 <= model.h && model.h < 360 then
-        { model | r = model.c, g = 0, b = model.x }
-
-    else
-        model
-
-
-unprime : Float -> Float -> Float
-unprime myColor m =
-    (myColor + m) * 255
-
-
-unprimeRGB : Model -> Model
-unprimeRGB model =
-    let
-        myR =
-            unprime model.r model.m
-
-        myG =
-            unprime model.g model.m
-
-        myB =
-            unprime model.b model.m
-    in
-    { model | r = myR, g = myG, b = myB }
-
-
 type Msg
-    = HueChange
-    | SaturationChange
-    | ValueChange
+    = ConvertBtnPressed Model
 
 
 update : Msg -> Model -> Model
 update msg model =
-    hsvToRgb model
+    case msg of
+        ConvertBtnPressed myModel ->
+            let
+                input =
+                    { h = myModel.h
+                    , s = myModel.s
+                    , v = myModel.v
+                    }
+
+                results =
+                    hsvToRgb input
+            in
+            { model
+                | r = results.r
+                , g = results.g
+                , b = results.b
+            }
 
 
-view : Model -> Html.Html Msg
 view model =
-    let
-        r =
-            String.fromFloat model.r
-
-        g =
-            String.fromFloat model.g
-
-        b =
-            String.fromFloat model.b
-    in
     Element.layout
         []
     <|
-        Element.column []
-            [ Element.el [] (text ("R: " ++ r ++ " G: " ++ g ++ " B: " ++ b))
+        Element.column [ width fill, spacing 20, padding 40 ]
+            [ Element.el
+                [ centerX ]
+                (text
+                    ("H: "
+                        ++ String.fromInt model.h
+                        ++ " S: "
+                        ++ String.fromFloat model.s
+                        ++ " V: "
+                        ++ String.fromFloat model.v
+                    )
+                )
+            , Element.el
+                [ centerX ]
+                (text
+                    ("R: "
+                        ++ String.fromInt model.r
+                        ++ " G: "
+                        ++ String.fromInt model.g
+                        ++ " B: "
+                        ++ String.fromInt model.b
+                    )
+                )
+            , Input.button
+                [ Background.color white
+                , Font.color black
+                , paddingXY 10 10
+                , Border.rounded 3
+                , centerX
+                ]
+                { onPress = Just <| ConvertBtnPressed model
+                , label = Element.text "Convert"
+                }
             ]
+
+
+white =
+    rgb255 255 255 255
+
+
+black =
+    rgb255 0 1 0
